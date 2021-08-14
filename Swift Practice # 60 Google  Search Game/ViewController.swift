7770d607f7722197595e62ec2cc647681dc11ae3
DogpaChen <dogpa0130@gmail.com>
2021-08-14 13:59:13 +0800

    Initial Commit

---
+++ Swift Practice # 60 Google  Search Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 60 Google  Search Game
+//
+//  Created by CHEN PEIHAO on 2021/8/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
